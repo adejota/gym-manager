5d9d7bd6b53e6e2c3d1e207cf6688f356006ce8c
adejota <ademir.diamentejunior@gmail.com>
2021-02-09 11:04:03 -0300

    Ajuste no config db para prod

--- src/config/db.js
+++ src/config/db.js
@@ -2,16 +2,16 @@ var mysql = require('mysql')
 
 module.exports = mysql.createConnection({
     // Desenvolvimento
-    user: "root",
-    password: "Ad_914989",
+    /* user: "root",
+    password: "********",
     host: "127.0.0.1",
     port: 3306,
-    database: "gym_manager"
+    database: "gym_manager" */
 
     // Produção
-    /* user: process.env.RDS_USERNAME,
+    user: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD,
     host: process.env.RDS_HOSTNAME,
     port: process.env.RDS_PORT,
-    database: process.env.RDS_DB_NAME */
+    database: process.env.RDS_DB_NAME
 })
